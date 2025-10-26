8d63aff51a4136757ae2d57fdbcacaad78a9229a
MRegisCT <maregina861@gmail.com>
2025-10-25 22:30:37 -0600

    Primer commit: estructura y environment conf

---
+++ src/app/features/player/components/player-main/player-main.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-player-main',
+  imports: [],
+  templateUrl: './player-main.html',
+  styleUrl: './player-main.css',
+})
+export class PlayerMain {
+
+}
---
+++ src/app/features/player/components/playlist-sidebar/playlist-sidebar.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-playlist-sidebar',
+  imports: [],
+  templateUrl: './playlist-sidebar.html',
+  styleUrl: './playlist-sidebar.css',
+})
+export class PlaylistSidebar {
+
+}
